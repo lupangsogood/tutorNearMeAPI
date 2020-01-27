e228be6895f2c87a5fe3b8dfdc883fe8bb72305d
Anusit Poyen <lupangsogood@gmail.com>
2020-01-27 17:03:42 +0700

    Improve Code

--- module/paymentController/paymentController.js
+++ module/paymentController/paymentController.js
@@ -124,7 +124,6 @@ const fetchPayment = (req, res) => {
   try {
     var user_id = req.body.user_id
     var role_id = req.body.role_id
-
     switch (role_id) {
       case "1":
         sqlFetchPayment = `SELECT payment_id,payment_date,payment_time,subject.subject_name_th,payment_amount,profile.profile_name,profile.profile_lastname,profile.profile_image,payment_image,paymentStatus_id,profile.role_id
@@ -133,13 +132,17 @@ const fetchPayment = (req, res) => {
                     INNER JOIN profile ON payment.payment_student_id = profile.profile_id
                     WHERE profile.profile_id = ${user_id}`
         break
-      default:
+      case "2":
         sqlFetchPayment = `SELECT payment_id,payment_date,payment_time,subject.subject_name_th,payment_amount,profile.profile_name,profile.profile_lastname,profile.profile_image,payment_image,paymentStatus_id,profile.role_id
                     FROM payment 
                     INNER JOIN subject ON payment.payment_course_id =subject.subject_id
                     INNER JOIN profile ON payment.payment_tutor_id = profile.profile_id
                     WHERE profile.profile_id = ${user_id}`
 
+        break
+
+      default:
+        console.log("ไม่พบค่า")
         break
     }
     try {
