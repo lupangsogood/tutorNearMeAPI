73572f4a8ca19ca58ea0784c600e3752984a3f01
lupin3rd <lupangsogood@gmail.com>
2020-06-03 01:50:37 +0700

    Update connectDB.js

--- connectDB.js
+++ connectDB.js
@@ -1,17 +1,10 @@
 const mysql =require('mysql')
 
-// var host = 'us-cdbr-iron-east-05.cleardb.net';
-// var user = 'b2995b4f6dc3b6'
-// var password = '6c76c5d7'
-// var dbName = 'heroku_ece53a02cb7485f'
-// var port = '3306';
-
-
-var host = 'erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-var user = 'jxni9mdvkc9n6ata'
-var password = 'htetxggfiaa655hu'
-var dbName = 'naxr8w21i6j7tqab'
-var port = '3306';
+var host = 'your-host';
+var user = ''
+var password = ''
+var dbName = ''
+var port = '';
 
 
 
